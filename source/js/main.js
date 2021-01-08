aea83584e5b4737a1ab5ad4ad57c1694dbb395a6
g5-freemen <member@tut.by>
2021-01-09 00:24:21 +0300

    fix: percents; feat: highlight pressed keys

--- source/js/main.js
+++ source/js/main.js
@@ -53,13 +53,15 @@ function Calculator(previousOperandText, currentOperandText, result) {
         if (!this.currentOperand) return;
         this.previousOperandText.innerText = `${this.expression}${this.currentOperand}%`;
         this.currentOperandText.innerText = '';
+        if (this.previousOperandText.innerText.includes('×')) this.operation = '*';
+        if (this.previousOperandText.innerText.includes('÷')) this.operation = '/';
+
         this.x = eval(
             parseFloat(this.expression) +
                 this.operation +
                 (parseFloat(this.expression) / 100) *
                     parseFloat(this.currentOperand)
         );
-
         this.result.innerText = this.x;
         this.currentOperand = '';
     };
@@ -74,6 +76,13 @@ function Calculator(previousOperandText, currentOperandText, result) {
             this.currentOperand += number.toString();
         }
     };
+
+    this.deleteDigit = function () {
+        if (this.currentOperand.length > 0) {
+            this.currentOperand = this.currentOperand.slice(0,length-1);
+            calculator.updateDisplay();
+        }
+    }
 
     this.reverse = function () {
         if (this.currentOperand.includes('-') ) {
@@ -98,9 +107,9 @@ function Calculator(previousOperandText, currentOperandText, result) {
     };
 
     this.compute = function () {
-        let computation;
-        const prev = parseFloat(this.previousOperand);
-        const current = parseFloat(this.currentOperand);
+        let computation,
+            prev = parseFloat(this.previousOperand),
+            current = parseFloat(this.currentOperand);
         if (isNaN(prev) || isNaN(current)) return;
 
         if (this.operation == '+') computation = prev + current;
@@ -109,7 +118,6 @@ function Calculator(previousOperandText, currentOperandText, result) {
         if (this.operation == '÷') computation = prev / current;
 
         this.resulting = computation;
-        console.log(this.resulting);
 
         this.operation = undefined;
         this.previousOperand = '';
@@ -122,9 +130,17 @@ function Calculator(previousOperandText, currentOperandText, result) {
         this.result.innerText = this.resulting;
     };
 }
+
+function highlightBtn(place) {
+    if (place) {
+    let eventBGcolor = place.style.background;
+    place.style.background = '#bbb';
+    setTimeout(() => place.style.background = eventBGcolor, 99);
+} }
+
 //#endregion
 
-setInterval(() => {
+setInterval(() => { // shows 0 if empty input string
     if (!currentOperandText.innerText) currentOperandText.innerText = '0';   
 }, 66);
 
@@ -134,21 +150,45 @@ let calculator = new Calculator( previousOperandText, currentOperandText, result
 
 //#region Keyboard Listener
 document.addEventListener('keydown', event => { // keyboard listener
+    if (event.key.match(/[1234567890\*\+/=%\.-]/) 
+        || event.keyCode == 13 || event.keyCode == 27 ) { // changes BG color of pressed button for 99 ms
+        let eventTarget;
+        if (event.key.match(/[1234567890\+\.=%-]/)) {
+            eventTarget = calcPad.querySelector(`[value="${event.key}"]`);
+        } else if (event.key == '/') {
+            eventTarget = calcPad.querySelector(`[value="÷"]`);
+        } else if (event.key == '*') {
+            eventTarget = calcPad.querySelector(`[value="×"]`);
+            this.operation == '×';
+        }
+        else if (event.keyCode == 13) eventTarget = calcPad.querySelector(`[value="="]`)
+        else if (event.keyCode == 27) eventTarget = calcPad.querySelector(`[value="C"]`)
+        highlightBtn(eventTarget);
+    };
+
     if (event.key == +event.key || event.key == '.' ) { // press digit or . button on keyboard
         calculator.appendNumber( event.key );
         calculator.updateDisplay();
 
     } else if (event.key.match(/[\*\+/-]/)) { // press +-/* button on keyboard
-        calculator.chooseOperation(event.key);
+        if (event.key == '/') calculator.chooseOperation('÷')
+        else if (event.key == '*') calculator.chooseOperation('×')
+        else calculator.chooseOperation(event.key);
         calculator.updateDisplay();
     
-    } else if (event.key == '=') { // press = button on keyboard
+    } else if (event.key == '=' || event.key == 'Enter') { // press = button on keyboard
         calculator.compute();
         calculator.showResult();
     
     } else if (event.key == 'Escape') { // press ESC button on keyboard
         calculator.clearAll();
         calculator.updateDisplay();
+    
+    } else if (event.key == 'Backspace') { // press Backspace (to delete last digit) on keyboard
+        calculator.deleteDigit();
+    
+    } else if (event.key == '%') { // press % button on keyboard
+        calculator.computePercent();
     }
 } ); 
 //#endregion
@@ -157,17 +197,15 @@ document.addEventListener('keydown', event => { // keyboard listener
 calcPad.addEventListener('click', event => { // mouse clicks listener
     let eventValue = event.target.value;
 
-    if (event.target.className.includes('button')) { // changes BG color of pressed button for 99 ms
-        let eventBGcolor = event.target.style.background;
-        event.target.style.background = '#bbb';
-        setTimeout(() => event.target.style.background = eventBGcolor, 99);
+    if (event.target.className.includes('button')) { // changes BG color of clicked button for 99 ms
+        highlightBtn(event.target);
     } else return;
 
     if (eventValue == +eventValue || eventValue == '.') { // click digit or .
         calculator.appendNumber(eventValue);
         calculator.updateDisplay();
 
-    } else if (event.target.className.includes('colorOper')) {  // click operation button
+    } else if (event.target.className.includes('colorOper')) {  // click +-/* button
         calculator.chooseOperation(eventValue);
         calculator.updateDisplay();
 
